14f48ab9ac6fe1ee46758da9366244be2906fcd3
LeDatBoi <94530456+LeDatBoi@users.noreply.github.com>
2021-12-01 10:19:35 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# eg-vil-gi-op-og-d-
+ 
---
+++ gay monkey.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+cout << "GoodBye World? \n\n"; //This tells the computer to print the text in "" and create a blank line beneath it
+cout << "I am trying to learn this godforsaken programing language" << endl;
+cout << "It could be going worse, i got some help from Emil /n/n" << endl;
+int myNum = 15;
+cout << myNum;
+int x= 5; int y = 6; // This tells the computer that x=5 and that y=6 and asks it to add them together and display the sum
+int sum = x + y; cout << sum;
+int a = 32, b = 16, c = 73;
+cout << a + b + c;
+int p,s;
+cout << "Type a number: "; // Type a number and press enter
+cin >> p; // Get user input from the keyboard
+cout << "Your number is: " << p; // Display the input value
+cout << " Type another number: ";
+cin >> s;
+sum = p + s;
+cout << "Sum is: " << sum;
+
+return 0;
+}
+
